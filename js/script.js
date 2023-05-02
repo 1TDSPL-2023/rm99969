0577010812f6de6b178e648c400f832f465c3e74
Ltheus <matheusglopes1@gmail.com>
2023-05-02 09:49:36 -0300

    atualizacao estilos e inicio js TITULO BALADA

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const h2Element = document.getElementById("titulo");
+h2Element.addEventListener("click", ()=>{
+    // alert("Baby, I'm preying on you tonight \nHunt you down eat you alive \nJust like animals \nAnimals \nLike animals-mals");
+    let r = ""
+    let g = ""
+    let b = ""
+    
+    r = Math.round(Math.random()*255)
+    g = Math.round(Math.random()*255)
+    b = Math.round(Math.random()*255)
+
+    h2Element.setAttribute("style", `color:rgb(${r},${g},${b});`);
+});
