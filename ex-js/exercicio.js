c35620ddf2504329d670c6c88be603f8476a285d
codingTheus <matheusglopes1@gmail.com>
2023-05-08 23:52:04 -0300

    QUASE tudo resolvido :(

--- ex-js/exercicio.js
+++ ex-js/exercicio.js
@@ -15,17 +15,6 @@ h2Element.addEventListener("click", ()=>{
 });
 
 //EX 2 - CRIAR UM FORM COM BOTÃO "ENVIAR" QUE CRIA UMA LISTA (WIP)
-const addInput = document.querySelector('#idNome');
-const divList = document.querySelector('#listHolder')
-
-const formElement = document.getElementById("form");
-formElement.addEventListener("submit", ()=>{
-    const ul = divList.querySelector('ul');
-    const li = document.createElement('li');
-    li.innerHTML = addInput.innerHTML;
-    addInput.value = '';
-    ul.appendChild(li);
-});
 
 //EX 3 -  CRIAR UM BOTÃO QUE TROCA UMA IMAGEM POR OUTRA
 const h3Element = document.querySelector('#imgBtn');
@@ -33,8 +22,14 @@ const h3Element = document.querySelector('#imgBtn');
 h3Element.addEventListener("click", ()=>{
     const imgElement = document.getElementById("imgEx3");
     imgElement.src = "../img/lobo1.jpg"
-    h3Element.innerHTML = "Clique para voltar a imagem"
+    h3Element.innerHTML = "Clique novamente para voltar a imagem"
+
+    h3Element.addEventListener("click", ()=>{
+        imgElement.src = "../img/logo.png"
+        h3Element.innerHTML = "Clique aqui para trocar de imagem"
+    });
 });
+
 
 //EX 4 - CRIAR UM BOTÃO QUE ADICIONA UMA MENSAGEM EM ALGUM ELEMENTO
 const changeBtn = document.getElementById("changeBtn");
@@ -42,4 +37,17 @@ const changeBtn = document.getElementById("changeBtn");
 changeBtn.addEventListener("click", ()=>{
     const msgElement = document.getElementById("msg");
     msgElement.innerHTML = "Número aleatório: " + Math.round(Math.random()*1000);
+});
+
+//EX 5 - CRIAR UM BOTÃO QUE OCULTA ALGUM ELEMENTO DA PÁGINA
+const hideBtn = document.getElementById("hideBtn");
+const gif = document.getElementById("gifEmoji")
+hideBtn.addEventListener("click", ()=>{
+    if (gifEmoji.style.display === "none") {
+        gifEmoji.style.display = "block";
+        hideBtn.value = "Clique aqui para esconder o gif abaixo!"
+      } else {
+        gifEmoji.style.display = "none";
+        hideBtn.value = "Clique novamente para voltar o gif!"
+      }
 });
